efd685ebf8ae92c217df11b254a28d7fe8e2533b
Alper <alpersarginercun@gmail.com>
2024-05-15 13:06:55 +0300

    map API

---
+++ app/src/main/java/com/example/voicetomap/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.voicetomap
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var mGoogleMap:GoogleMap? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.mapFragment) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mGoogleMap = googleMap
+    }
+}
