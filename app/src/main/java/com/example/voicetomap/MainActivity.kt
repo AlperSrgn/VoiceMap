54085875813f52dfb05af17779e7d7f3f945a001
Alper <alpersarginercun@gmail.com>
2024-05-16 14:24:01 +0300

    current location

--- app/src/main/java/com/example/voicetomap/MainActivity.kt
+++ app/src/main/java/com/example/voicetomap/MainActivity.kt
@@ -1,27 +1,72 @@
 package com.example.voicetomap
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    private var mGoogleMap:GoogleMap? = null
+class MainActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+    private lateinit var mGoogleMap: GoogleMap
+    private lateinit var lastLocation: Location
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    companion object{
+        private const val LOCATION_REQUEST_CODE = 1
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.mapFragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         mGoogleMap = googleMap
+        mGoogleMap.uiSettings.isZoomControlsEnabled = true
+        mGoogleMap.setOnMarkerClickListener ( this )
+        setUpMap()
     }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_REQUEST_CODE)
+            return
+        }
+        mGoogleMap.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener(this){ location ->
+
+            if(location!= null){
+                lastLocation = location
+                val current_Lat_Long = LatLng(location.latitude, location.longitude)
+                placeMarkerOnMap(current_Lat_Long)
+                mGoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(current_Lat_Long, 10f))
+            }
+        }
+    }
+
+    private fun placeMarkerOnMap(current_Lat_Long: LatLng) {
+        val markerOptions = MarkerOptions().position(current_Lat_Long)
+        markerOptions.title("$current_Lat_Long")
+        mGoogleMap.addMarker(markerOptions)
+    }
+
+    override fun onMarkerClick(p0: Marker) = false
 }
